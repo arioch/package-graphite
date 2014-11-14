b493f5dea70cc04e0fe14ff813be5f8c0ecba823
Tom De Vylder <tom@penumbra.be>
2014-11-14 10:16:37 +0100

    Bugfix: --rpm-os required when building on OSX

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ if [ ! -f python-carbon-${PKG_VERSION}-1.noarch.rpm ]
 then
   wget http://launchpad.net/graphite/0.9/${PKG_VERSION}/+download/carbon-${PKG_VERSION}.tar.gz
   tar xvzf carbon-${PKG_VERSION}.tar.gz
-  fpm --python-install-bin /opt/graphite/bin -s python -t rpm carbon-${PKG_VERSION}/setup.py
+  fpm --rpm-os linux --python-install-bin /opt/graphite/bin -s python -t rpm carbon-${PKG_VERSION}/setup.py
   rm -rf carbon-${PKG_VERSION}.tar.gz
   rm -rf carbon-${PKG_VERSION}
 fi
@@ -45,7 +45,7 @@ if [ ! -f python-graphite-web-${PKG_VERSION}-1.noarch.rpm ]
 then
   wget http://launchpad.net/graphite/0.9/${PKG_VERSION}/+download/graphite-web-${PKG_VERSION}.tar.gz
   tar xvzf graphite-web-${PKG_VERSION}.tar.gz
-  fpm --python-install-lib /opt/graphite/webapp -s python -t rpm graphite-web-${PKG_VERSION}/setup.py
+  fpm --rpm-os linux --python-install-lib /opt/graphite/webapp -s python -t rpm graphite-web-${PKG_VERSION}/setup.py
   rm -rf graphite-web-${PKG_VERSION}.tar.gz
   rm -rf graphite-web-${PKG_VERSION}
 fi
@@ -54,7 +54,7 @@ if [ ! -f python-whisper-${PKG_VERSION}-1.noarch.rpm ]
 then
   wget https://launchpad.net/graphite/0.9/${PKG_VERSION}/+download/whisper-${PKG_VERSION}.tar.gz
   tar xvzf whisper-${PKG_VERSION}.tar.gz
-  fpm --python-install-lib /opt/graphite/bin -s python -t rpm whisper-${PKG_VERSION}/setup.py
+  fpm --rpm-os linux --python-install-lib /opt/graphite/bin -s python -t rpm whisper-${PKG_VERSION}/setup.py
   rm -rf whisper-${PKG_VERSION}.tar.gz
   rm -rf whisper-${PKG_VERSION}
 fi
